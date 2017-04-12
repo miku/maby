6bc3410832e7ad8ff323ea7fa14b01572384d207
Martin Czygan <martin.czygan@gmail.com>
2017-04-12 16:39:40 +0200

    mabread defaults to a json output

--- cmd/mabread/main.go
+++ cmd/mabread/main.go
@@ -1,30 +1,34 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
 	"log"
 	"os"
 
 	"github.com/miku/maby"
 )
 
+var stripCollection = flag.Bool("strip", false, "strip collation bytes (88, 89)")
+
 func main() {
+	flag.Parse()
 	r := maby.NewReader(os.Stdin)
-	records, err := r.ReadRecords()
-	if err != nil {
-		log.Fatal(err)
+	r.StripCollation = *stripCollection
+	for {
+		rec, err := r.ReadRecord()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		b, err := json.Marshal(rec)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(b))
 	}
-	log.Printf("%d records", len(records))
-	// r.StripCollation = true
-	// for {
-	// 	rec, err := r.ReadRecord()
-	// 	if err == io.EOF {
-	// 		break
-	// 	}
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	if f, ok := rec.FieldByKey("331"); ok {
-	// 		fmt.Println(f.Value)
-	// 	}
-	// }
 }
